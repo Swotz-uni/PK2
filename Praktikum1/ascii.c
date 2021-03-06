534a6359e39fbacf5338d8d22a72931496f3423f
Swotz-uni <sven-siebe@gmx.de>
2021-03-06 09:24:29 +0100

    Bug Fixed - count to counter

--- Praktikum1/ascii.c
+++ Praktikum1/ascii.c
@@ -4,7 +4,7 @@ int ascii(int s)
     int counter = 0;
     for (int i = 32;i<=126;i++)
     {
-        count++;
+        counter++;
         if(counter%s==0)
             printf("%3d %c\n",i,i);
         else
